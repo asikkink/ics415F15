aea39d50e100fea6b2f36785fa64e31a7ce6d6f1
asikkink <asikkink@hawaii.edu>
2015-09-20 15:08:58 -1000

    A07 work
    stuff

---
+++ A07/myScript.js
@@ -0,0 +1,15 @@
+getClasses(elem){
+  var classes = elem.className;
+  /*split substr by spaces*/
+  /*save to an array*/
+}
+
+addClass(elem, className){
+  var classes = getClasses(elem);
+  /*check the array to see if className is in the list*/
+  /*if it is, don't add it, otherwise add the classes from elem.className along with the new className*/
+}
+
+validateForm(){
+/* checks that all text fields have entries and that the password and confirm password entries are the same. If there are any errors have the validateForm */
+}
