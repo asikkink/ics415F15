459da8251b6d1eb145c04912a79c782eb6b7db67
asikkink <asikkink@hawaii.edu>
2015-09-21 09:32:54 -1000

    Completed in-class assignment for week 5 class 1

---
+++ IC/w5c1/javascript.js
@@ -0,0 +1,37 @@
+$(function() {
+
+  //add image if col1 is clicked
+  $("#col1").click(function () {
+    $(this).html("<img src='unicorn2.jpg' style='height:300px'>");
+  });
+
+  //add image if col2 is clicked
+  $("#col2").hover(function () {
+    $(this).html("<img src='3unicorn3.jpg' style='height:300px'>");
+  });
+
+  //on mouseenter on col5, show bg image
+  $("#col5").mouseenter(function(){
+    $("body").css("background-image", "url('unicron.jpg')");
+    $("body").height("100%");
+  });
+
+  //on mouse leave, remove the bg image
+  $("#col5").mouseleave(function(){
+    $("body").css("background-image", "");
+    $("body").height("100%");
+  });
+
+  //change col1's image when col3 is clicked
+  $("#col3").click(function(){
+    $("#col1").html("<img src='3unicorn3.jpg' style='height:300px'>");
+  });
+
+    //change col2's image when col4 is double clicked
+  $("#col4").dblclick(function(){
+    $("#col2").html("<img src='unicorn2.jpg' style='height:300px'>");
+  });
+
+
+
+});
